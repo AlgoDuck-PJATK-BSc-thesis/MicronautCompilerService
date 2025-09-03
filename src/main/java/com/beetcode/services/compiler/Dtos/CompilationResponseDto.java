e253e8c1d5aad4609a74bfb81b5f2b8c7c9b6c8d
janek <s27920@pjwstk.edu.pl>
2025-09-03 22:50:37 +0200

    feat: added entrypoint file name to returned dto

--- src/main/java/com/beetcode/services/compiler/Dtos/CompilationResponseDto.java
+++ src/main/java/com/beetcode/services/compiler/Dtos/CompilationResponseDto.java
@@ -9,9 +9,11 @@ import java.util.Map;
 @Serdeable
 @Getter
 public class CompilationResponseDto extends CompilationResponseAbstr {
+    private final String entrypoint;
     private final Map<String, String> generatedClassFiles;
 
-    public CompilationResponseDto(Map<String, String> generatedClassFiles) {
+    public CompilationResponseDto(String entrypoint, Map<String, String> generatedClassFiles) {
+        this.entrypoint = entrypoint;
         this.generatedClassFiles = generatedClassFiles;
     }
 }
